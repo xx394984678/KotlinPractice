bd2415544032b9e458f0e5036a7e24c3babfd2a5
lirui <18513396869@163.com>
2019-11-14 23:13:41 +0800

    初步构建项目

---
+++ app/src/main/java/com/lirui/kotlinpracticeapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.lirui.kotlinpracticeapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
+fun main() {
+    println(
+        "some thing"
+    )
+}
